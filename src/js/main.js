6b36ec9cb2dbd3212620e7ad34af962c8b77a769
Gabriela Czekala <gabrielaczekala@gmail.com>
2023-04-02 23:01:49 +0200

    main page started

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../scss/elements/main.scss';
+
+const Main = () => {
+    return (
+        <div className="container">
+            <div className="main-container">
+                <div className="main-img"></div>
+                <nav className="main-links">
+                    <div className="main-link">
+                        <h1>Create your template</h1>
+                    </div>
+                    <div className="secondary-links">
+
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+
+}
+
+export default Main
